d76310d7873c6b794396d28fdf4d67270b404982
Adrian Heouairi <adrian.heouairi@gmail.com>
2023-12-07 22:22:36 +0100

    Added request of peers

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const SERVER_ADDRESS = "https://jch.irif.fr:8443"
+const PEERS_PATH = "/peers/"
+var LOGGING_FUNC = log.Println
 
 func main() {
-    fmt.Println("abc")
+    req, err := http.NewRequest("GET", SERVER_ADDRESS + PEERS_PATH, nil)
+    if err != nil { LOGGING_FUNC("Error") }
+
+    client := &http.Client{}
+	resp, err := client.Do(req)
+    if err != nil { LOGGING_FUNC("Error") }
+
+    respAsByteSlice, err := ioutil.ReadAll(resp.Body)
+    if err != nil { LOGGING_FUNC("Error") }
+	respBodyStr := string(respAsByteSlice)
+
+    fmt.Println(respBodyStr)
 }
