498d208055fe719ef25fa7e458629fa219fe4799
Sevi Dervishi <sevidervishi@outlook.fr>
2023-12-09 17:26:06 +0100

    Need to finish hello sending.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # PSI-project
-+ recuperer les adresses des pairs connecte et se faire conaitre en temps que pair dispo
++ recuperer la liste des pairs DONE
++ recuperer les adresses des pairs connecte DONE
++ recuperer la racine des pairs connectés DONE
++ commencer la communication UDP
+    1. la structure message
+    2. traiter hello
+    3. :
+
+
+
++ se faire conaitre en temps que pair dispo TODO
 
--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+    "math/rand"
+	"encoding/binary"
+	"net"
 )
+
+type udpSocket struct {
+	Host string
+	Port int
+}
+
+type msg struct {
+	Id     uint32
+	Type   uint8
+	Length uint16
+	Body   []byte
+}
 
 const SERVER_ADDRESS = "https://jch.irif.fr:8443"
 const PEERS_PATH = "/peers/"
@@ -90,11 +105,63 @@ func getRootOfPeer(peerName string)  [] byte{
 	fmt.Println("root of " + peerName + " is : " + respBodyStr)
     return respAsByteSlice
 }
+func toByteSlice(toCast msg) []byte {
+	var idToByteSlice []byte = make([]byte, 4)
+	binary.LittleEndian.PutUint32(idToByteSlice, toCast.Id)
+	var typeToByteSlice []byte = make([]byte, 1)
+	typeToByteSlice[0] = toCast.Type
+	var lengthToByteSlice []byte = make([]byte, 2)
+	binary.BigEndian.PutUint16(lengthToByteSlice, toCast.Length)
+	var res = append(idToByteSlice, typeToByteSlice...)
+	res = append(res, lengthToByteSlice...)
+	res = append(res, toCast.Body...)
+	return res
+}
+
+func createHello() []byte {
+    var helloMsg msg
+    helloMsg.Id = rand.Uint32()
+    helloMsg.Type = 2
+    extention := make([] byte, 4)
+    name := "tc"
+    nameAsBytes := [] byte(name)
+    var res = append(extention, nameAsBytes...)
+    helloMsg.Length = uint16(len(res))
+    return toByteSlice(helloMsg)
+}
 
 func main() {
-	getPeers()
+	/*getPeers()
 	getAdressOfPeer("jch.irif.fr")
 	getAdressOfPeer("jch.irsif.fr")
 	getRootOfPeer("jch.irsif.fr")
 	getRootOfPeer("Slartibartfast")
+*/
+	var sock udpSocket
+    sock.Host = SERVER_ADDRESS[8:19]
+    fmt.Println(sock.Host)
+    sock.Port =  8443
+	// Server address
+	serverAddr, err := net.ResolveUDPAddr("udp", sock.Host+":"+fmt.Sprint(sock.Port))
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+	// Establish a connection
+	conn, err := net.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+
+	// Send the UDP packet
+	_, err = conn.Write(createHello())
+	if err != nil {
+		fmt.Println("Error sending UDP packet:", err)
+		return
+	}
+    
 }
