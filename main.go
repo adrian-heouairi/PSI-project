8b5c3f286adbeb0d3e00eeebccd4d8105d1a3808
Adrian Heouairi <adrian.heouairi@gmail.com>
2023-12-09 22:49:31 +0100

    Added UDP communication with server

--- main.go
+++ main.go
@@ -1,111 +1,47 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/binary"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
-    "math/rand"
-	"encoding/binary"
+	"math/rand"
 	"net"
+	"net/http"
+	"strings"
 )
 
-type udpSocket struct {
-	Host string
-	Port int
-}
+const SERVER_ADDRESS = "https://jch.irif.fr:8443"
+const PEERS_PATH = "/peers/"
+const OUR_PEER_NAME = "AS"
+const SERVER_PEER_NAME = "jch.irif.fr"
+var LOGGING_FUNC = log.Println
 
-type msg struct {
+const (
+    NOOP byte = 0
+    ERROR byte = 1
+    ERROR_REPLY byte = 128
+    HELLO byte = 2
+    HELLO_REPLY byte = 129
+    PUBLIC_KEY byte = 3
+    PUBLIC_KEY_REPLY byte = 130
+    ROOT byte = 4
+    ROOT_REPLY byte = 131
+    GET_DATUM byte = 5
+    DATUM byte = 132
+    NO_DATUM byte = 133
+    NAT_TRAVERSAL_REQUEST byte = 6
+    NAT_TRAVERSAL byte = 7
+)
+
+type udpMsg struct {
 	Id     uint32
 	Type   uint8
 	Length uint16
 	Body   []byte
 }
-
-const SERVER_ADDRESS = "https://jch.irif.fr:8443"
-const PEERS_PATH = "/peers/"
-
-var LOGGING_FUNC = log.Println
-
-func getPeers() {
-	req, err := http.NewRequest("GET", SERVER_ADDRESS+PEERS_PATH, nil)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-
-	respAsByteSlice, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-	respBodyStr := string(respAsByteSlice)
-
-	fmt.Println(respBodyStr)
-
-}
-
-func getAdressOfPeer(peerName string) {
-
-	req, err := http.NewRequest("GET", SERVER_ADDRESS+
-		PEERS_PATH+"/"+peerName+"/addresses", nil)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-
-	if resp.StatusCode == 404 {
-		fmt.Println(peerName + "is not known by server!")
-		return 
-	}
-	respAsByteSlice, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-	respBodyStr := string(respAsByteSlice)
-
-	fmt.Println(respBodyStr)
-}
-
-func getRootOfPeer(peerName string)  [] byte{
-
-	req, err := http.NewRequest("GET", SERVER_ADDRESS+PEERS_PATH+"/"+peerName+"/root", nil)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-
-	if resp.StatusCode == 204 {
-		fmt.Println(peerName + " has not declared root yet!")
-		return make([]byte, 0)
-	} else if resp.StatusCode == 404 {
-		fmt.Println(peerName + "is not known by server!")
-		return make([]byte, 0)
-	}
-	respAsByteSlice, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		LOGGING_FUNC("Error")
-	}
-	respBodyStr := string(respAsByteSlice)
-
-	fmt.Println("root of " + peerName + " is : " + respBodyStr)
-    return respAsByteSlice
-}
-func toByteSlice(toCast msg) []byte {
+func udpMsgToByteSlice(toCast udpMsg) []byte {
 	var idToByteSlice []byte = make([]byte, 4)
 	binary.LittleEndian.PutUint32(idToByteSlice, toCast.Id)
 	var typeToByteSlice []byte = make([]byte, 1)
@@ -117,17 +53,70 @@ func toByteSlice(toCast msg) []byte {
 	res = append(res, toCast.Body...)
 	return res
 }
+func byteSliceToUdpMsg(toCast []byte) udpMsg {
+	var m udpMsg
+	m.Id = binary.LittleEndian.Uint32(toCast[0:4])
+	m.Type = toCast[4]
+	m.Length = binary.LittleEndian.Uint16(toCast[5:7])
+	m.Body = toCast[7:7 + m.Length]
+	return m
+}
 
-func createHello() []byte {
-    var helloMsg msg
+func httpGet(url string) (*http.Response, []byte) {
+    resp, err := http.Get(url)
+    if err != nil {
+		LOGGING_FUNC(err)
+	}
+
+    bodyAsByteSlice, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		LOGGING_FUNC(err)
+	}
+
+    return resp, bodyAsByteSlice
+}
+
+func getPeers() {
+    _, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH)
+	fmt.Println(string(bodyAsByteSlice))
+}
+
+func getAdressesOfPeer(peerName string) []string {
+    resp, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH + "/" + peerName + "/addresses")
+
+	if resp.StatusCode == 404 {
+		LOGGING_FUNC(peerName + " is not known by server")
+		return make([]string, 0)
+	}
+	
+    return strings.Split(string(bodyAsByteSlice), "\n")
+}
+
+func getRootOfPeer(peerName string) []byte {
+    resp, bodyAsByteSlice := httpGet(SERVER_ADDRESS+PEERS_PATH+"/"+peerName+"/root")
+
+	if resp.StatusCode == 204 {
+		LOGGING_FUNC(peerName + " has not declared a root yet!")
+		return make([]byte, 0)
+	} else if resp.StatusCode == 404 {
+		LOGGING_FUNC(peerName + "is not known by server!")
+		return make([]byte, 0)
+	}
+	
+    return bodyAsByteSlice
+}
+
+func createHello() udpMsg {
+    var helloMsg udpMsg
     helloMsg.Id = rand.Uint32()
     helloMsg.Type = 2
-    extention := make([] byte, 4)
-    name := "tc"
-    nameAsBytes := [] byte(name)
-    var res = append(extention, nameAsBytes...)
+    extensions := make([]byte, 4)
+    name := OUR_PEER_NAME
+    nameAsBytes := []byte(name)
+    var res = append(extensions, nameAsBytes...)
+    helloMsg.Body = res
     helloMsg.Length = uint16(len(res))
-    return toByteSlice(helloMsg)
+    return helloMsg
 }
 
 func main() {
@@ -137,31 +126,77 @@ func main() {
 	getRootOfPeer("jch.irsif.fr")
 	getRootOfPeer("Slartibartfast")
 */
-	var sock udpSocket
-    sock.Host = SERVER_ADDRESS[8:19]
-    fmt.Println(sock.Host)
-    sock.Port =  8443
-	// Server address
-	serverAddr, err := net.ResolveUDPAddr("udp", sock.Host+":"+fmt.Sprint(sock.Port))
+
+    serverUdpAddresses := getAdressesOfPeer(SERVER_PEER_NAME)
+
+    // Server address
+	serverAddr, err := net.ResolveUDPAddr("udp", serverUdpAddresses[0])
 	if err != nil {
-		fmt.Println("Error resolving address:", err)
-		return
+		LOGGING_FUNC(err)
 	}
 
 	// Establish a connection
 	conn, err := net.DialUDP("udp", nil, serverAddr)
 	if err != nil {
-		fmt.Println("Error connecting:", err)
-		return
+		LOGGING_FUNC(err)
 	}
 	defer conn.Close()
 
-
-	// Send the UDP packet
-	_, err = conn.Write(createHello())
-	if err != nil {
-		fmt.Println("Error sending UDP packet:", err)
-		return
-	}
+	buffer := make([]byte, 1048576)
     
+    helloMsg := createHello()
+
+    _, err = conn.Write(udpMsgToByteSlice(helloMsg))
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
+
+    _, _, err = conn.ReadFromUDP(buffer)
+    if err != nil {
+		LOGGING_FUNC(err)
+	}
+
+    helloReplyMsg := byteSliceToUdpMsg(buffer)
+
+    if helloMsg.Id != helloReplyMsg.Id || helloReplyMsg.Type != HELLO_REPLY {
+        LOGGING_FUNC("Invalid HelloReply message")
+    }
+
+    _, _, err = conn.ReadFromUDP(buffer)
+    if err != nil {
+		LOGGING_FUNC(err)
+	}
+
+    publicKeyMsg := byteSliceToUdpMsg(buffer)
+
+    if publicKeyMsg.Type != PUBLIC_KEY {
+        LOGGING_FUNC("Invalid PublicKey message")
+    }
+
+    publicKeyReplyMsg := udpMsg{publicKeyMsg.Id, PUBLIC_KEY_REPLY, 0, make([]byte, 0)}
+
+    _, err = conn.Write(udpMsgToByteSlice(publicKeyReplyMsg))
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
+
+    _, _, err = conn.ReadFromUDP(buffer)
+    if err != nil {
+		LOGGING_FUNC(err)
+	}
+
+    rootMsg := byteSliceToUdpMsg(buffer)
+
+    if rootMsg.Type != ROOT {
+        LOGGING_FUNC("Invalid Root message")
+    }
+
+    hasher := sha256.New()
+    //h.Write([]byte(""))
+    rootReplyMsg := udpMsg{rootMsg.Id, ROOT_REPLY, 32, hasher.Sum(nil)}
+
+    _, err = conn.Write(udpMsgToByteSlice(rootReplyMsg))
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 }
