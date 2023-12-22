05ae5cec29cd3212883e850960ccff33bc4a94f1
Sevi Dervishi <sevidervishi@outlook.fr>
2023-12-23 00:30:36 +0100

    Implemented a functions that extracts the names and hashes of a directory elements.

--- main.go
+++ main.go
@@ -20,106 +20,109 @@ const SERVER_PEER_NAME = "jch.irif.fr"
 var LOGGING_FUNC = log.Println
 
 const (
-	NOOP                  byte = 0
-	ERROR                 byte = 1
-	ERROR_REPLY           byte = 128
-	HELLO                 byte = 2
-	HELLO_REPLY           byte = 129
-	PUBLIC_KEY            byte = 3
-	PUBLIC_KEY_REPLY      byte = 130
-	ROOT                  byte = 4
-	ROOT_REPLY            byte = 131
-	GET_DATUM             byte = 5
-	DATUM                 byte = 132
-	NO_DATUM              byte = 133
-	NAT_TRAVERSAL_REQUEST byte = 6
-	NAT_TRAVERSAL         byte = 7
+    NOOP                  byte = 0
+    ERROR                 byte = 1
+    ERROR_REPLY           byte = 128
+    HELLO                 byte = 2
+    HELLO_REPLY           byte = 129
+    PUBLIC_KEY            byte = 3
+    PUBLIC_KEY_REPLY      byte = 130
+    ROOT                  byte = 4
+    ROOT_REPLY            byte = 131
+    GET_DATUM             byte = 5
+    DATUM                 byte = 132
+    NO_DATUM              byte = 133
+    NAT_TRAVERSAL_REQUEST byte = 6
+    NAT_TRAVERSAL         byte = 7
+    CHUNK                 byte = 0
+    TREE                  byte = 1
+    DIRECTORY             byte = 2
 )
 
 type udpMsg struct {
-	Id     uint32
-	Type   uint8
-	Length uint16
-	Body   []byte
+    Id     uint32
+    Type   uint8
+    Length uint16
+    Body   []byte
 }
 
 func udpMsgToByteSlice(toCast udpMsg) []byte {
-	var idToByteSlice []byte = make([]byte, 4)
-	binary.BigEndian.PutUint32(idToByteSlice, toCast.Id)
-	var typeToByteSlice []byte = make([]byte, 1)
-	typeToByteSlice[0] = toCast.Type
-	var lengthToByteSlice []byte = make([]byte, 2)
-	binary.BigEndian.PutUint16(lengthToByteSlice, toCast.Length)
-	var res = append(idToByteSlice, typeToByteSlice...)
-	res = append(res, lengthToByteSlice...)
-	res = append(res, toCast.Body...)
-	return res
+    var idToByteSlice []byte = make([]byte, 4)
+    binary.BigEndian.PutUint32(idToByteSlice, toCast.Id)
+    var typeToByteSlice []byte = make([]byte, 1)
+    typeToByteSlice[0] = toCast.Type
+    var lengthToByteSlice []byte = make([]byte, 2)
+    binary.BigEndian.PutUint16(lengthToByteSlice, toCast.Length)
+    var res = append(idToByteSlice, typeToByteSlice...)
+    res = append(res, lengthToByteSlice...)
+    res = append(res, toCast.Body...)
+    return res
 }
 
 func byteSliceToUdpMsg(toCast []byte) udpMsg {
-	var m udpMsg
-	m.Id = binary.BigEndian.Uint32(toCast[0:4])
-	m.Type = toCast[4]
-	m.Length = binary.BigEndian.Uint16(toCast[5:7])
-	m.Body = append([]byte{}, toCast[7 : 7+m.Length]...)
-	return m
+    var m udpMsg
+    m.Id = binary.BigEndian.Uint32(toCast[0:4])
+    m.Type = toCast[4]
+    m.Length = binary.BigEndian.Uint16(toCast[5:7])
+    m.Body = append([]byte{}, toCast[7 : 7+m.Length]...)
+    return m
 }
 
 func httpGet(url string) (*http.Response, []byte) {
-	resp, err := http.Get(url)
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    resp, err := http.Get(url)
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
-	bodyAsByteSlice, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    bodyAsByteSlice, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
-	return resp, bodyAsByteSlice
+    return resp, bodyAsByteSlice
 }
 
 func getPeers() {
-	_, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH)
-	fmt.Println(string(bodyAsByteSlice))
+    _, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH)
+    fmt.Println(string(bodyAsByteSlice))
 }
 
 func getAdressesOfPeer(peerName string) []string {
-	resp, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH + "/" + peerName + "/addresses")
+    resp, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH + "/" + peerName + "/addresses")
 
-	if resp.StatusCode == 404 {
-		LOGGING_FUNC(peerName + " is not known by server")
-		return make([]string, 0)
-	}
+    if resp.StatusCode == 404 {
+        LOGGING_FUNC(peerName + " is not known by server")
+        return make([]string, 0)
+    }
 
-	return strings.Split(string(bodyAsByteSlice), "\n")
+    return strings.Split(string(bodyAsByteSlice), "\n")
 }
 
 func getRootOfPeer(peerName string) []byte {
-	resp, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH + "/" + peerName + "/root")
+    resp, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH + "/" + peerName + "/root")
 
-	if resp.StatusCode == 204 {
-		LOGGING_FUNC(peerName + " has not declared a root yet!")
-		return make([]byte, 0) // TODO Fix this and other instances of returning wrong value after logging (maybe exit?)
-	} else if resp.StatusCode == 404 {
-		LOGGING_FUNC(peerName + "is not known by server!")
-		return make([]byte, 0)
-	}
+    if resp.StatusCode == 204 {
+        LOGGING_FUNC(peerName + " has not declared a root yet!")
+        return make([]byte, 0) // TODO Fix this and other instances of returning wrong value after logging (maybe exit?)
+    } else if resp.StatusCode == 404 {
+        LOGGING_FUNC(peerName + "is not known by server!")
+        return make([]byte, 0)
+    }
 
-	return bodyAsByteSlice
+    return bodyAsByteSlice
 }
 
 func createHello() udpMsg {
-	var helloMsg udpMsg
-	helloMsg.Id = rand.Uint32()
-	helloMsg.Type = 2
-	extensions := make([]byte, 4)
-	name := OUR_PEER_NAME
-	nameAsBytes := []byte(name)
-	var res = append(extensions, nameAsBytes...)
-	helloMsg.Body = res
-	helloMsg.Length = uint16(len(res))
-	return helloMsg
+    var helloMsg udpMsg
+    helloMsg.Id = rand.Uint32()
+    helloMsg.Type = 2
+    extensions := make([]byte, 4)
+    name := OUR_PEER_NAME
+    nameAsBytes := []byte(name)
+    var res = append(extensions, nameAsBytes...)
+    helloMsg.Body = res
+    helloMsg.Length = uint16(len(res))
+    return helloMsg
 }
 
 func createMsg(msgType byte, msgBody []byte) udpMsg {
@@ -137,85 +140,83 @@ func udpMsgToString(msg udpMsg) string {
     if lengthToTake > 32 {
         lengthToTake = 32
     }
-/*
+    var typeAsString string
+    switch msg.Type {
+    case 0:
+        typeAsString = "NOOP"
+    case 1:
+        typeAsString = "ERROR"
+    case 128:
+        typeAsString = "ERROR REPLY"
+    case 2:
+        typeAsString = "HELLO"
+    case 129:
+        typeAsString = "HELLO REPLY"
+    case 3:
+        typeAsString = "PUBLIC KEY"
+    case 130:
+        typeAsString = "PUBLIC KEY REPLY"
+    case 4:
+        typeAsString = "ROOT"
+    case 131:
+        typeAsString = "ROOT REPLY"
+    case 5:
+        typeAsString = "GET DATUM"
+    case 132:
+        typeAsString = "DATUM"
+    case 6:
+        typeAsString = "NAT TRAVERSAL REQUEST"
+    case 133:
+        typeAsString = "NO DATUM"
+    case 7:
+        typeAsString = "NAT TRAVERSAL"
 
-	NOOP                  byte = 0
-	ERROR                 byte = 1
-	ERROR_REPLY           byte = 128
-	HELLO                 byte = 2
-	HELLO_REPLY           byte = 129
-	PUBLIC_KEY            byte = 3
-	PUBLIC_KEY_REPLY      byte = 130
-	ROOT                  byte = 4
-	ROOT_REPLY            byte = 131
-	GET_DATUM             byte = 5
-	DATUM                 byte = 132
-	NO_DATUM              byte = 133
-	NAT_TRAVERSAL_REQUEST byte = 6
-	NAT_TRAVERSAL         byte = 7
-*/
-var typeAsString string
-switch msg.Type {
-case 0:
-    typeAsString = "NOOP"
-case 1:
-    typeAsString = "ERROR"
-case 128:
-    typeAsString = "ERROR REPLY"
-case 2:
-    typeAsString = "HELLO"
-case 129:
-    typeAsString = "HELLO REPLY"
-case 3:
-    typeAsString = "PUBLIC KEY"
-case 130:
-    typeAsString = "PUBLIC KEY REPLY"
-case 4:
-    typeAsString = "ROOT"
-case 131:
-    typeAsString = "ROOT REPLY"
-case 5:
-    typeAsString = "GET DATUM"
-case 132:
-    typeAsString = "DATUM"
-case 6:
-    typeAsString = "NAT TRAVERSAL REQUEST"
-case 133:
-    typeAsString = "NO DATUM"
-case 7:
-    typeAsString = "NAT TRAVERSAL"
-    
-}
+    }
     return "Id: " + fmt.Sprint(msg.Id) + "\n" +
-        "Type: " + typeAsString + "\n" +
-        "Length: " + fmt.Sprint(msg.Length) + "\n" +
-        "Body: " + string(msg.Body[:lengthToTake])
+    "Type: " + typeAsString + "\n" +
+    "Length: " + fmt.Sprint(msg.Length) + "\n" +
+    "Body: " + string(msg.Body[:lengthToTake])
 }
 
+// Returns a map containing the names and the hashes of the Datum message
+// Assumes that msg is datum of type Directory
+// Otherwise empty map is returned
+func getDataHashes(msg udpMsg) map[string][]byte {
+    res := make(map[string][]byte)
+    if msg.Body[32] == DIRECTORY {
+        fmt.Println("Cool its a directory");
+        nbEntry := (msg.Length - 32) / 64
+        for i := 0; i < int(nbEntry); i++{
+            res[string(msg.Body[33 + i * 32 : 33 + i * 32 + 32])] = msg.Body[65:97]  
+        }
+        fmt.Println(nbEntry) 
+    }
+    return res
+}
 func main() {
-	/*getPeers()
-	getAdressOfPeer("jch.irif.fr")
-	getAdressOfPeer("jch.irsif.fr")
-	getRootOfPeer("jch.irsif.fr")
-	getRootOfPeer("Slartibartfast")
-	*/
+    /*getPeers()
+    getAdressOfPeer("jch.irif.fr")
+    getAdressOfPeer("jch.irsif.fr")
+    getRootOfPeer("jch.irsif.fr")
+    getRootOfPeer("Slartibartfast")
+    */
 
-	serverUdpAddresses := getAdressesOfPeer(SERVER_PEER_NAME)
+    serverUdpAddresses := getAdressesOfPeer(SERVER_PEER_NAME)
 
-	// Server address
-	serverAddr, err := net.ResolveUDPAddr("udp", serverUdpAddresses[0])
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    // Server address
+    serverAddr, err := net.ResolveUDPAddr("udp", serverUdpAddresses[0])
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
-	// Establish a connection
-	conn, err := net.DialUDP("udp", nil, serverAddr)
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
-	defer conn.Close()
+    // Establish a connection
+    conn, err := net.DialUDP("udp", nil, serverAddr)
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
+    defer conn.Close()
 
-	buffer := make([]byte, 1048576)
+    buffer := make([]byte, 1048576)
 
     sendAndReceiveMsg := func (toSend udpMsg) udpMsg {
         _, err = conn.Write(udpMsgToByteSlice(toSend))
@@ -238,61 +239,61 @@ func main() {
         return replyMsg
     }
 
-	helloMsg := createHello()
+    helloMsg := createHello()
 
-	_, err = conn.Write(udpMsgToByteSlice(helloMsg))
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    _, err = conn.Write(udpMsgToByteSlice(helloMsg))
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
-	_, _, err = conn.ReadFromUDP(buffer)
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    _, _, err = conn.ReadFromUDP(buffer)
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
-	helloReplyMsg := byteSliceToUdpMsg(buffer)
+    helloReplyMsg := byteSliceToUdpMsg(buffer)
 
-	if helloMsg.Id != helloReplyMsg.Id || helloReplyMsg.Type != HELLO_REPLY {
-		LOGGING_FUNC("Invalid HelloReply message")
-	}
+    if helloMsg.Id != helloReplyMsg.Id || helloReplyMsg.Type != HELLO_REPLY {
+        LOGGING_FUNC("Invalid HelloReply message")
+    }
 
-	_, _, err = conn.ReadFromUDP(buffer)
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    _, _, err = conn.ReadFromUDP(buffer)
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
-	publicKeyMsg := byteSliceToUdpMsg(buffer)
+    publicKeyMsg := byteSliceToUdpMsg(buffer)
 
-	if publicKeyMsg.Type != PUBLIC_KEY {
-		LOGGING_FUNC("Invalid PublicKey message")
-	}
+    if publicKeyMsg.Type != PUBLIC_KEY {
+        LOGGING_FUNC("Invalid PublicKey message")
+    }
 
-	publicKeyReplyMsg := udpMsg{publicKeyMsg.Id, PUBLIC_KEY_REPLY, 0, make([]byte, 0)}
+    publicKeyReplyMsg := udpMsg{publicKeyMsg.Id, PUBLIC_KEY_REPLY, 0, make([]byte, 0)}
 
-	_, err = conn.Write(udpMsgToByteSlice(publicKeyReplyMsg))
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    _, err = conn.Write(udpMsgToByteSlice(publicKeyReplyMsg))
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
-	_, _, err = conn.ReadFromUDP(buffer)
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    _, _, err = conn.ReadFromUDP(buffer)
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
-	rootMsg := byteSliceToUdpMsg(buffer)
+    rootMsg := byteSliceToUdpMsg(buffer)
 
-	if rootMsg.Type != ROOT {
-		LOGGING_FUNC("Invalid Root message")
-	}
+    if rootMsg.Type != ROOT {
+        LOGGING_FUNC("Invalid Root message")
+    }
 
-	hasher := sha256.New()
-	//h.Write([]byte(""))
-	rootReplyMsg := udpMsg{rootMsg.Id, ROOT_REPLY, 32, hasher.Sum(nil)}
+    hasher := sha256.New()
+    //h.Write([]byte(""))
+    rootReplyMsg := udpMsg{rootMsg.Id, ROOT_REPLY, 32, hasher.Sum(nil)}
 
-	_, err = conn.Write(udpMsgToByteSlice(rootReplyMsg))
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
+    _, err = conn.Write(udpMsgToByteSlice(rootReplyMsg))
+    if err != nil {
+        LOGGING_FUNC(err)
+    }
 
 
 
@@ -306,4 +307,20 @@ func main() {
     rootDatumReply := sendAndReceiveMsg(createMsg(GET_DATUM, rootJuliuszUDP))
     // Parser racine (directory)
     fmt.Println(udpMsgToString(rootDatumReply))
+    if rootDatumReply.Body[32] == DIRECTORY {
+        fmt.Println("Cool its a directory");
+        
+    }
+    fmt.Println("premier elt de la racine de Juliusz : " + string(rootDatumReply.Body[33:65]))
+    hashOfFirstElt := rootDatumReply.Body[65:97]
+    mr = sendAndReceiveMsg(createMsg(GET_DATUM,hashOfFirstElt))
+    if mr.Type == DATUM{
+        fmt.Println("its a datum msg of lenght : " + fmt.Sprint(mr.Length))
+    }
+    if mr.Body[32] == CHUNK {
+        fmt.Println("its a chunk")
+        fmt.Println("content : " + string(mr.Body[33:mr.Length]))
+    }
+    elt := getDataHashes(rootDatumReply)
+    fmt.Println(elt)
 }
