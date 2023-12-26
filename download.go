78e7f3e8518d5ad45bfed6433efd02d53c9631f4
Sevi Dervishi <sevidervishi@outlook.fr>
2023-12-26 09:43:09 +0100

    Adrian your turn compelete funcs with error.

--- download.go
+++ download.go
@@ -4,24 +4,33 @@ import (
 	"fmt"
 )
 
-func lsRecursive(hash []byte) {
-    datumReply := sendAndReceiveMsg(createMsg(GET_DATUM, hash))
+func lsRecursive(hash []byte, depth int)(error) {
+    datumReply,err := sendAndReceiveMsg(createMsg(GET_DATUM, hash))
+    if err != nil {
+        return err
+
+    }
     datumType, datumToCast := parseDatum(datumReply.Body)
     
     if (datumType == DIRECTORY) {
         datum := datumToCast.(datumDirectory)
 
-        fmt.Print("Map : ")
-        fmt.Println(datum.Children)
+        //fmt.Print("Map : ")
+        //fmt.Println(datum.Children)
 
-        for _, value := range datum.Children {
-            lsRecursive(value)
+        for key, value := range datum.Children {
+            for i := 0; i < depth; i++ {
+                fmt.Print("\t")
+            }
+            fmt.Println(key)
+            lsRecursive(value,depth+1)
         }
     }
+    return nil
 }
 
 func listAllFilesOfPeer(peer string) {
     RESTPeerRootHash := getRootOfPeer(peer)
     
-    lsRecursive(RESTPeerRootHash)
+    lsRecursive(RESTPeerRootHash,0)
 }
--- main.go
+++ main.go
@@ -5,11 +5,7 @@ import (
 	//"fmt"
 	//"log"
 	"net"
-	"net/http"
-	"strings"
-    "time"
-    "sync"
-    "strconv"
+	"os"
 )
 
 var jchConn *net.UDPConn
@@ -20,63 +16,14 @@ func createDownloadDirAndCd() {
 		err = os.Mkdir(DOWNLOAD_DIR, 0755)
 		checkErr(err)
 	}
-
-    bodyAsByteSlice, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		LOGGING_FUNC(err)
-	}
-
-    return resp, bodyAsByteSlice
+	err = os.Chdir(DOWNLOAD_DIR)
+	checkErr(err)
 }
 
-func getPeers() []string{
-    _, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH)
-    listOfPeersAsString := string(bodyAsByteSlice)
-    listOfPeers := strings.Split(listOfPeersAsString,"\n")
-    printNumberedList(listOfPeers)
-    return listOfPeers
-}
+func main() {
+	createDownloadDirAndCd()
 
-func getAdressesOfPeer(peerName string) []string {
-    resp, bodyAsByteSlice := httpGet(SERVER_ADDRESS + PEERS_PATH + "/" + peerName + "/addresses")
-
-	if resp.StatusCode == 404 {
-		LOGGING_FUNC(peerName + " is not known by server")
-		return make([]string, 0)
-	}
-	
-    return strings.Split(string(bodyAsByteSlice), "\n")
-}
-
-func getRootOfPeer(peerName string) []byte {
-    resp, bodyAsByteSlice := httpGet(SERVER_ADDRESS+PEERS_PATH+"/"+peerName+"/root")
-
-	if resp.StatusCode == 204 {
-		LOGGING_FUNC(peerName + " has not declared a root yet!")
-		return make([]byte, 0)
-	} else if resp.StatusCode == 404 {
-		LOGGING_FUNC(peerName + "is not known by server!")
-		return make([]byte, 0)
-	}
-	
-    return bodyAsByteSlice
-}
-
-func createHello() udpMsg {
-    var helloMsg udpMsg
-    helloMsg.Id = rand.Uint32()
-    helloMsg.Type = 2
-    extensions := make([]byte, 4)
-    name := OUR_PEER_NAME
-    nameAsBytes := []byte(name)
-    var res = append(extensions, nameAsBytes...)
-    helloMsg.Body = res
-    helloMsg.Length = uint16(len(res))
-    return helloMsg
-}
-
-func keepConnectionAlive() {
-    serverUdpAddresses := getAdressesOfPeer(SERVER_PEER_NAME)
+	serverUdpAddresses := getAdressesOfPeer(SERVER_PEER_NAME)
 
 	// Server address
 	serverAddr, err := net.ResolveUDPAddr("udp", serverUdpAddresses[0])
@@ -87,10 +34,14 @@ func keepConnectionAlive() {
 	checkErr(err)
 	defer jchConn.Close()
 
-	buffer := make([]byte, 1048576)
-    
-    helloMsg := createHello()
-for {
+	sendAndReceiveMsg(createHello()) // TODO Check that it is a HelloReply
+	publicKeyMsg := receiveMsg()
+	publicKeyReplyMsg := createMsgWithId(publicKeyMsg.Id, PUBLIC_KEY_REPLY, []byte{})
+	sendMsg(publicKeyReplyMsg)
+	rootMsg := receiveMsg()
+	hasher := sha256.New()
+	rootReplyMsg := createMsgWithId(rootMsg.Id, ROOT_REPLY, hasher.Sum(nil))
+	sendMsg(rootReplyMsg)
 
 	listAllFilesOfPeer("jch.irif.fr")
 
@@ -152,97 +103,7 @@ for {
 		rootDatumReply.Body[32:rootDatumReply.Length]) {
 		LOGGING_FUNC("DATUM IS CORRUPTED")
 	}
-<<<<<<< HEAD
-
-    publicKeyMsg := byteSliceToUdpMsg(buffer)
-
-    if publicKeyMsg.Type != PUBLIC_KEY {
-        LOGGING_FUNC("Invalid PublicKey message")
-    }
-
-    publicKeyReplyMsg := udpMsg{publicKeyMsg.Id, PUBLIC_KEY_REPLY, 0, make([]byte, 0)}
-
-    _, err = conn.Write(udpMsgToByteSlice(publicKeyReplyMsg))
-    if err != nil {
-        LOGGING_FUNC(err)
-    }
-
-    _, _, err = conn.ReadFromUDP(buffer)
-    if err != nil {
-		LOGGING_FUNC(err)
-	}
-
-    rootMsg := byteSliceToUdpMsg(buffer)
-
-    if rootMsg.Type != ROOT {
-        LOGGING_FUNC("Invalid Root message")
-    }
-
-    hasher := sha256.New()
-    rootReplyMsg := udpMsg{rootMsg.Id, ROOT_REPLY, 32, hasher.Sum(nil)}
-
-    _, err = conn.Write(udpMsgToByteSlice(rootReplyMsg))
-    if err != nil {
-        LOGGING_FUNC(err)
- }
-     time.Sleep(30 * time.Second) 
-     fmt.Println("After waiting 30 seconds")
- }
-}
-func printNumberedList(list []string) {
-    for i:= 0 ; i < len(list) - 1; i++{
-        fmt.Println(strconv.Itoa(i + 1) + " - " + list[i])
-    }
-=======
 	hashes := getDataHashes(rootDatumReply)
 	fmt.Println(hashes)
 	download_and_save_file("images", rootDatumReply.Body[:32], []byte{})*/
->>>>>>> 3f298843e7df187b0f67fbe7ff84e7159c458fd2
-}
-func UI() {
-    fmt.Println("PEER CLIENT")
-    fmt.Println("1 - Get peers list")
-    fmt.Println("2 - Get addresses of a peer")
-    fmt.Println("3 - Get root of a peer")
-    var i int
-
-    fmt.Print("Type a number[1..3]: ")
-    fmt.Scan(&i)
-    switch i {
-    case 1:
-        fmt.Println("Here is the list of peers :")
-        getPeers()
-    case 2:
-        listOfPeers := getPeers()
-        fmt.Println("Which pair are you interesseted by[1.." + strconv.Itoa(len(listOfPeers)) + "] :")
-        fmt.Scan(&i)
-        fmt.Println(listOfPeers[i-1] + " addresses are : ")
-        printNumberedList(getAdressesOfPeer(listOfPeers[i-1]))
-    }
-    fmt.Println("Your number is:", i)
-}
-
-func main() {
-UI()
-    /*
-    PAY ATTENTION THIS CODE COMES FROM CHATGPT AND NEED TO BE REFACTORED
-    */
-var wg sync.WaitGroup
-
-	// Start the goroutine and increment the WaitGroup counter
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		keepConnectionAlive()
-	}()
-
-	// Your other main function logic...
-
-	// Wait for the goroutine to finish before exiting
-	wg.Wait()
-	fmt.Println("Program exited")
-go keepConnectionAlive()
-/*
-END OF CHATGPT CODE
-*/
 }
