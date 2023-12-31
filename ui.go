88a9e57ed9830dda3788acb25a2c1c721d406beb
Sevi Dervishi <sevidervishi@outlook.fr>
2023-12-31 22:05:16 +0100

    Replaced exit by constants.

--- ui.go
+++ ui.go
@@ -12,105 +12,94 @@ var helpMessage = fmt.Sprintf(
 	`CMD [OPTION]
     - %s : shows the connected peers
     - %s PATH : downloads the datum at the PATH asumes that the path is absolute i.e contains peer name
-    - %s PEER : shows the files shared by PEER`, LIST_PEERS_CMD, DOWNLOAD_FILE_CMD, LIST_FILES_CMD)
+    - %s PEER : shows the files shared by PEER
+    - %s quit`, LIST_PEERS_CMD, DOWNLOAD_FILE_CMD, LIST_FILES_CMD, EXIT_CMD)
+    
 
-/*func autoCompleteSuggestion() []string {
-  var commands = []string{LIST_PEERS_CMD, LIST_FILES_CMD, DOWNLOAD_FILE_CMD, "exit"}  
-  line := d.GetWordBeforeCursor()
-	if line == "" {
-		return commands
-	}
+    func peersList() []string {
+        peers, _ := restGetPeers(false)
+        return peers
+    }
+    func parseLine(line string) {
+        line = strings.TrimSpace(line)
+        line = replaceAllRegexBy(line, " +", " ")
+        splitLine := strings.Split(line, " ")
 
-	var suggestions []string
-	for _, command := range commands {
-		if strings.HasPrefix(command, line) {
-			suggestions = append(suggestions, command)
-		}
-	}
+        if len(splitLine) == 0 {
+            fmt.Println(helpMessage)
+        }
 
-	return suggestions
-}
-*/ 
-func parseLine(line string) {
-	line = strings.TrimSpace(line)
-	line = replaceAllRegexBy(line, " +", " ")
-	splitLine := strings.Split(line, " ")
+        switch splitLine[0] {
+        case EXIT_CMD:
+            os.Exit(0)
+        case LIST_PEERS_CMD:
+            restGetPeers(true)
+        case LIST_FILES_CMD:
+            if len(splitLine) < 2 {
+                fmt.Fprintln(os.Stderr, helpMessage)
+            } else {
+                filenamesAndHashes, err := getPeerAllDataHashes(splitLine[1])
+                if err != nil {
+                    fmt.Println(err)
+                }
+                availableFiles := getKeys(filenamesAndHashes)
+                for _, elt := range availableFiles {
+                    fmt.Println(elt)
 
-	if len(splitLine) == 0 {
-		fmt.Println(helpMessage)
-	}
+                }
+            }
+            //case CAT_FILE_CMD:
+        case DOWNLOAD_FILE_CMD:
+            if len(splitLine) < 2 {
+                fmt.Fprintln(os.Stderr, helpMessage)
+            } else if len(splitLine) == 2 { // We assume that splitLine[1] is <peer>/<path>
+                path := removeTrailingSlash(splitLine[1])
+                // TODO : support peers whose name contains /
+                peerName := replaceAllRegexBy(path, "/.*", "")
+                filenamesAndHashes, err := getPeerAllDataHashes(peerName)
+                if err != nil {
+                    fmt.Println(err)
+                }
+                val, found := filenamesAndHashes[path]
+                if found {
+                    downloadRecursive(peerName, val, path)
 
-	switch splitLine[0] {
-    case EXIT_CMD:
-        os.Exit(0)
-	case LIST_PEERS_CMD:
-		restGetPeers(true)
-	case LIST_FILES_CMD:
-		if len(splitLine) < 2 {
-			fmt.Fprintln(os.Stderr, helpMessage)
-		} else {
-			filenamesAndHashes, err := getPeerAllDataHashes(splitLine[1])
-			if err != nil {
-				fmt.Println(err)
-			}
-			availableFiles := getKeys(filenamesAndHashes)
-			for _, elt := range availableFiles {
-				fmt.Println(elt)
+                } else {
+                    fmt.Println("NOT FOUND")
+                    fmt.Println(path)
+                }
+            }
 
-			}
-		}
-	//case CAT_FILE_CMD:
-	case DOWNLOAD_FILE_CMD:
-		if len(splitLine) < 2 {
-			fmt.Fprintln(os.Stderr, helpMessage)
-		} else if len(splitLine) == 2 { // We assume that splitLine[1] is <peer>/<path>
-			path := removeTrailingSlash(splitLine[1])
-			// TODO : support peers whose name contains /
-			peerName := replaceAllRegexBy(path, "/.*", "")
-			filenamesAndHashes, err := getPeerAllDataHashes(peerName)
-			if err != nil {
-				fmt.Println(err)
-			}
-			val, found := filenamesAndHashes[path]
-			if found {
-				downloadRecursive(peerName, val, path)
+            default: // Includes HELP_CMD
+            fmt.Fprintln(os.Stderr, helpMessage)
+        }
+    }
 
-			} else {
-				fmt.Println("NOT FOUND")
-				fmt.Println(path)
-			}
-		}
+    func mainMenu() error {
+        var completer = readline.NewPrefixCompleter(
+            //readline.PcItem(LIST_PEERS_CMD, readline.PcItemDynamic(peersList)),
+            readline.PcItem(LIST_FILES_CMD),
+            readline.PcItem(DOWNLOAD_FILE_CMD),
+            readline.PcItem(EXIT_CMD))
+            rl, err := readline.NewEx(&readline.Config{
+                UniqueEditLine: true,
+                Prompt: CLI_PROMPT,
+                InterruptPrompt: "^C",
+                EOFPrompt: "exit",
+                HistoryFile: "/tmp/readlinehistory.tmp",
+                AutoComplete: completer,
+            })
 
-	default: // Includes HELP_CMD
-		fmt.Fprintln(os.Stderr, helpMessage)
-	}
-}
+            if err != nil {
+                return err
+            }
+            defer rl.Close()
+            for {
+                line, err := rl.Readline()
+                if err != nil { // io.EOF
+                    return err
+                }
 
-func mainMenu() error {
-	var completer = readline.NewPrefixCompleter(
-        readline.PcItem(LIST_PEERS_CMD),
-        readline.PcItem(LIST_FILES_CMD),
-        readline.PcItem(DOWNLOAD_FILE_CMD),
-        readline.PcItem("exit"))
-	rl, err := readline.NewEx(&readline.Config{
-        UniqueEditLine: true,
-        Prompt: CLI_PROMPT,
-        InterruptPrompt: "^C",
-        EOFPrompt: "exit",
-        HistoryFile: "/tmp/readlinehistory.tmp",
-        AutoComplete: completer,
-    })
-
-	if err != nil {
-		return err
-	}
-	defer rl.Close()
-	for {
-		line, err := rl.Readline()
-		if err != nil { // io.EOF
-			return err
-		}
-
-		parseLine(line)
-	}
-}
+                parseLine(line)
+            }
+        }
