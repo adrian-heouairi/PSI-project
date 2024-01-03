c5105c0eed16ba36d34a91ce1a7968ff16f48efa
Adrian Heouairi <adrian.heouairi@gmail.com>
2024-01-03 17:17:16 +0100

    Fix hang on Hello to ourself

--- udp.go
+++ udp.go
@@ -186,10 +186,12 @@ func handleMsg(receivedMsg addrUdpMsg) {
 		helloReply, _ := createComplexHello(receivedMsg.Msg.Id, HELLO_REPLY)
 		simpleSendMsgToAddr(receivedMsg.Addr, helloReply)
 		parsedHello, _ := parseHello(receivedMsg.Msg.Body)
-		_, err = sendToAddrAndReceiveMsgWithReemissions(receivedMsg.Addr, createHello())
-		if err == nil {
-			// We don't add even if SOFT error
-			peersAddAddr(parsedHello.PeerName, receivedMsg.Addr)
+		if parsedHello.PeerName != OUR_PEER_NAME {
+			_, err = sendToAddrAndReceiveMsgWithReemissions(receivedMsg.Addr, createHello())
+			if err == nil {
+				// We don't add even if SOFT error
+				peersAddAddr(parsedHello.PeerName, receivedMsg.Addr)
+			}
 		}
 		return
 	case PUBLIC_KEY:
